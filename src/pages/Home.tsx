dc592eaf35085ed78efb5bb98163ba1399f3325c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-06-23 13:01:10 -0300

    Google authentication with Firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,17 @@ import googleIcon from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
 
-    function navigateToNewRoom() {
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
         history.push('/rooms/new');
     }
 
@@ -25,7 +31,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIcon} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
